c23c49be6fefcdf336694c322f5c5f5b4caad0e9
huangyuzhen <email@example.com>
2019-09-01 10:30:34 +0800

    小数背包的贪心算法

---
+++ 5.4_a.py
@@ -0,0 +1,40 @@
+'''
+小数背包，贪心算法
+优先装单位重量价值最高的物品
+'''
+
+Value = (25, 24, 15)
+Weight = (18,15,10)
+
+
+
+def greedyKnapsack(n, capacity):
+    itemList = []
+    for i in range(n):
+        item = {
+            "v": Value[i],
+            "w": Weight[i]
+        }
+        itemList.append(item)
+
+    def sortByRate(a):
+        return a['v'] / a['w']
+
+    itemList.sort(key = sortByRate, reverse = True)
+
+    answer = 0
+    for i in range(n):
+        item = itemList[i]
+        if capacity >= item['w']:
+            answer   += item['v']
+            capacity -= item['w']
+        else:
+            answer += capacity * (item['v'] / item['w'])
+            break
+
+    return answer
+
+n = len(Value)
+L = greedyKnapsack(n, 20)
+
+print(L)
