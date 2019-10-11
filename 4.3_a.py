e7e3a9a2c4a7bcf6b2dbeadf3286878c92d8c649
huangyuzhen <email@example.com>
2019-10-11 23:30:43 +0800

    矩阵连乘，自底向上算法

--- 4.3_a.py
+++ 4.3_a.py
@@ -3,37 +3,30 @@
 '''
 
 P = (30, 35, 15, 5, 10, 20, 25)
-
-
-def showSize(P):
-    A = {}
-    for i in range(1, len(P)):
-        A[i] = (P[i-1], P[i])
-        print("A{} {}".format(i, A[i]))
-
-
 matrixNum = len(P)
 
 memoryTable = {}
 bestK = {}
+
 for i in range(1, matrixNum):
     memoryTable[(i, i)] = 0
-    bestK[(i,i)] = i
 
-for length in range(2, matrixNum):
-    for i in range(1, matrixNum - length + 1):
-        j = i + length - 1
+for length in range(1, matrixNum):
+    for i in range(1, matrixNum - length):
+        j = i + length
         key = (i,j)
         memoryTable[key] = 100000000
         for k in range(i, j):
-            ans = memoryTable[(i,k)]
-            ans+= memoryTable[(k+1,j)]
-            ans+= P[i-1] * P[k] * P[j]
+            ans1 = memoryTable[(i,k)]
+            ans2 = memoryTable[(k+1,j)]
+            ans  = ans1 + ans2 + P[i-1] * P[k] * P[j]
             if ans < memoryTable[key]:
                 memoryTable[key] = ans
                 bestK[key] = k
 
+print(memoryTable[(1,matrixNum-1)])
 
+# 输出memoryTable
 print("memoryTable:")
 print(" ", end='')
 for i in range(1, matrixNum):
