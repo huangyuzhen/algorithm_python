a8c25de934052022c0e4b614751b98b7a871adc6
huangyuzhen <email@example.com>
2019-10-11 23:02:25 +0800

    矩阵连乘，递归＋备忘录

--- 4.4_b.py
+++ 4.4_b.py
@@ -4,34 +4,31 @@
 备忘录方法
 '''
 
-def showSize(P):
-    A = {}
-    for i in range(1, len(P)):
-        A[i] = (P[i-1], P[i])
-        print("A{} {}".format(i, A[i]))
-
 P = (30, 35, 15, 5, 10, 20, 25)
 memoryTable = {} #备忘录
+bestK = {}
 
 def matrixChain(i, j):
     key = (i,j)
-    if memoryTable.get(key):
-        return memoryTable.get(key)
-
-    if i == j:
+    if i >= j:
         memoryTable[key] = 0
         return 0
 
+    if memoryTable.get(key):
+        return memoryTable.get(key)
+
     minV = 1000000000
     for k in range(i,j):
-        answer = matrixChain(i,k)
-        answer+= matrixChain(k+1, j)
-        answer+= P[i-1] * P[k] * P[j]
+        answer1 = matrixChain(i,k)
+        answer2 = matrixChain(k+1, j)
+        answer  = answer1 + answer2 + P[i-1] * P[k] * P[j]
+        # print(i,k,j, answer)
         if answer < minV:
             minV = answer
+            bestK[key] = k
 
     memoryTable[key] = minV
-    #print((i,j), minV)
+    # print(key, minV)
     return minV
 
 
@@ -39,6 +36,7 @@ matrixNum = len(P)
 m = matrixChain(1, matrixNum - 1)
 print(m)
 
+# 输出memoryTable
 print("memoryTable:")
 print(" ", end='')
 for i in range(1, matrixNum):
@@ -53,3 +51,5 @@ for i in range(1, matrixNum):
         else:
             print("{:10d}".format(memoryTable[key]), end = '')
     print()
+
+print(bestK)
