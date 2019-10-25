0baab0ac9da4259c96a2bfa01199754ef6e3c0e7
huangyuzhen <email@example.com>
2019-10-25 22:30:34 +0800

    数字删除问题

--- 5_test_1.py
+++ 5_test_1.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
 def greedyNumber(number, k):
+    n = len(number)
+    if k >= n:
+        return ""
+
     for _ in range(k):
-        n = len(number)
         for i in range(n-1):
             if number[i] < number[i+1]:
                 number = number[:i] + number[i+1:]
                 break
         else:
             number = number[:-1]
+        n -= 1
 
     return number
 
 
-number = input("input number: ")
-k = int(input("input k: "))
+line = input().split()
+n = str(line[0])
+k = int(line[1])
 
-print(greedyNumber(number,k))
+number = greedyNumber(n, k)
+print(number)
